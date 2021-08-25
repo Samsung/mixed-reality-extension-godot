9971ebc55fbead83e25deb1e59ad4b7acfda4343
Yeongjong Lee <cleanlyj@naver.com>
2021-08-25 13:53:22 +0900

    add key and mouse interaction

---
+++ MREGodotRuntime/Scripts/ThumbTip.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class ThumbTip : MeshInstance
+{
+    Spatial indexTip;
+    public override void _Ready()
+    {
+        indexTip = ((Spatial)GetNode("../../../../IndexMetacarpal/IndexProximal/IndexIntermediate/IndexDistal/IndexTip"));
+    }
+
+    public override void _Input(InputEvent ev)
+    {
+        if (Input.IsActionPressed("Fire2"))
+        {
+            GlobalTransform = indexTip.GlobalTransform;
+        }
+        else if (Input.IsActionJustReleased("Fire2"))
+        {
+            Transform = new Transform(Transform.basis, new Vector3(0, 0, -0.05f));
+        }
+    }
+}
