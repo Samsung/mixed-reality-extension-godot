b39dc0b8347d6db6ef7f6987bf0871c3757d9d51
Yeongjong Lee <cleanlyj@naver.com>
2021-09-15 10:18:14 +0900

    add proximity light

--- addons/godot-openxr/scenes/IndexTip.cs
+++ addons/godot-openxr/scenes/IndexTip.cs
@@ -5,10 +5,14 @@ public class IndexTip : MeshInstance
 {
     bool pressed = false;
     AnimationPlayer animationPlayer;
+    RayCast rayCast;
+    OmniLight ProximityLight;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        rayCast = GetNode<RayCast>("RayCast");
+        ProximityLight = GetNode<OmniLight>("ProximityLight");
     }
 
     public override void _Input(InputEvent ev)
@@ -24,9 +28,21 @@ public class IndexTip : MeshInstance
             animationPlayer.PlayBackwards("touch");
         }
     }
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//
-//  }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        if (rayCast.IsColliding())
+        {
+            if (ProximityLight.Visible == false) ProximityLight.Visible = true;
+            GD.Print(rayCast.GetCollisionPoint().DistanceSquaredTo(rayCast.GlobalTransform.origin));
+            
+            //ProximityLight.OmniRange = 0.
+            ProximityLight.GlobalTransform = new Transform(ProximityLight.GlobalTransform.basis, rayCast.GetCollisionPoint() + rayCast.GetCollisionNormal().Normalized() * 0.01f);
+        }
+        else
+        {
+            GD.Print(pressed);
+            ProximityLight.Visible = pressed;
+        }
+    }
 }
