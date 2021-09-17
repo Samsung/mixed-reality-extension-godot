d8dfc190e9c3834bf7f43ebe6e2fa0d2bf59cb2b
Yeongjong Lee <cleanlyj@naver.com>
2021-09-17 16:17:48 +0900

    split the Godot Button specific stuff

--- addons/MREGodot/MRENodePlugin.cs
+++ addons/MREGodot/MRENodePlugin.cs
@@ -9,8 +9,15 @@ public class MRENodePlugin : EditorPlugin
         var script = GD.Load<Script>("MREGodotRuntime/Scripts/LaunchMRE.cs");
         var texture = GD.Load<Texture>("addons/MREGodot/icon.svg");
         AddCustomType("MRENode", "Spatial", script, texture);
+
+        var textScript = GD.Load<Script>("MREGodotRuntime/Scripts/SimpleText.cs");
+        AddCustomType("SimpleText", "MeshInstance", textScript, null);
+
         var buttonScript = GD.Load<Script>("Toolkit/PressableButton.cs");
         AddCustomType("PressableButton", "Spatial", buttonScript, null);
+
+        var buttonGodotScript = GD.Load<Script>("Toolkit/PressableButtonGodot.cs");
+        AddCustomType("PressableButtonGodot", "PressableButton", buttonScript, null);
 
         var interactionScript = GD.Load<Script>("Toolkit/NearInteractionTouchable.cs");
         AddCustomType("NearInteractionTouchable", "Spatial", interactionScript, null);
@@ -19,7 +26,9 @@ public class MRENodePlugin : EditorPlugin
     public override void _ExitTree()
     {
         RemoveCustomType("MRENode");
+        RemoveCustomType("SimpleText");
         RemoveCustomType("PressableButton");
+        RemoveCustomType("PressableButtonGodot");
         RemoveCustomType("NearInteractionTouchable");
     }
 }
