085a10262829a90d0794c7fc22c5ad4497214a31
Yeongjong Lee <cleanlyj@naver.com>
2021-10-26 14:43:08 +0900

    rename from HandTrackingInputEventData to TouchInputEventData

---
+++ Toolkit/IMixedRealityEventHandler.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using MixedRealityExtension.Behaviors.Actions;
+using Godot;
+using System;
+
+namespace Microsoft.MixedReality.Toolkit.Input
+{
+    public interface IMixedRealityEventHandler
+    {
+        public static T FindEventHandler<T>(Node node) where T : class
+        {
+            if (node is T EventHandler)
+                return EventHandler;
+
+            foreach (Node child in node.GetChildren())
+            {
+                var touchableChild = FindEventHandler<T>(child);
+                if (touchableChild != null) return touchableChild;
+            }
+            return null;
+        }
+    }
+}
