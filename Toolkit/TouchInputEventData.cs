f9bfea322a8d40a92c22e8cda3f6052a8c037256
Yeongjong Lee <cleanlyj@naver.com>
2021-10-26 14:43:08 +0900

    rename from HandTrackingInputEventData to TouchInputEventData

---
+++ Toolkit/TouchInputEventData.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Assets.Scripts.Tools;
+using Godot;
+
+namespace Microsoft.MixedReality.Toolkit.Input
+{
+    public class TouchInputEventData : InputEventData<Vector3>
+    {
+        public TouchInputEventData(Tool tool, Vector3 data) : base(tool, data)
+        {
+
+        }
+    }
+}
