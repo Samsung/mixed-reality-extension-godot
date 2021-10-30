474371f0ea00261eb0c07ae61cc1a8d80a5d0395
Yeongjong Lee <cleanlyj@naver.com>
2021-10-30 23:54:49 +0900

    emit pointer event when button tool is updated
    The ButtonTool is one of Tools that triggers pointer events.

---
+++ Toolkit/MixedRealityPointerEventData.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Assets.Scripts.Tools;
+using Godot;
+
+namespace Microsoft.MixedReality.Toolkit.Input
+{
+    public class MixedRealityPointerEventData : InputEventData<Vector3>
+    {
+        public MixedRealityPointerEventData(Tool tool, Vector3 data) : base(tool, data)
+        {
+
+        }
+    }
+}
