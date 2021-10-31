d7d84a66bf72b90209f17bbad4faef7383e41b5c
Yeongjong Lee <cleanlyj@naver.com>
2021-10-31 18:43:16 +0900

    clean up hand node and scripts
    The ProximityLight is created in runtime.

--- Toolkit/HighlightArea.cs
+++ Toolkit/HighlightArea.cs
@@ -1,16 +1,7 @@
 using Godot;
-using System;
 
 public class HighlightArea : Area
 {
-	[Signal]
-	public delegate void focused();
-
-	[Signal]
-	public delegate void unfocused();
-
-	ShaderMaterial HighlightPlateMaterial;
-
 	public static Vector3 BorderColor = new Vector3(0.42f, 0.48f, 0.61f);
 
 	public const string BorderColorString = "border_color";
