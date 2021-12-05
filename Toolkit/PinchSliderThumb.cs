6a01f05493f63905638e5beb7e73a62417d0910d
Yeongjong Lee <cleanlyj@naver.com>
2021-12-06 00:44:14 +0900

    toolkit: integrate spheretool into grabtool
    The SphereTool and the GrabTool have same feature which is grabbing the objects.
    so, integrate SphereTool into GrabTool.

--- Toolkit/PinchSliderThumb.cs
+++ Toolkit/PinchSliderThumb.cs
@@ -1,8 +1,36 @@
 using Godot;
+using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.UI;
 using MixedRealityExtension.Patching.Types;
 
-internal class PinchSliderThumb : Spatial, IToolkit
+internal class PinchSliderThumb : Spatial, IToolkit, IMixedRealityPointerHandler
 {
+    private PinchSlider slider;
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        slider.OnPointerUp(eventData);
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        slider.OnPointerDown(eventData);
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        slider.OnPointerDragged(eventData);
+    }
+
+    public override void _Ready()
+    {
+        this.RegisterHandler<IMixedRealityPointerHandler>();
+    }
+
+    public override void _EnterTree()
+    {
+        var actor = GetParent();
+        slider = actor.GetParent() as PinchSlider;
+    }
+
     public void ApplyPatch(ToolkitPatch toolkitPatch) {}
 }
