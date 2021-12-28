cf7cd030035258439d6065ec3541f3f6ff5946c8
Yeongjong Lee <cleanlyj@naver.com>
2021-12-28 15:57:18 +0900

    toolkit: Simplify button code
     - remove `origin`, `backward` uniform to reduce coupling between cs and
       shader
     - fix pulse visual
     - fix a issue that the closest normal was wrong in poke pointer

--- Toolkit/HighlightArea.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-
-public class HighlightArea : Area
-{
-	public static Vector3 BorderColor = new Vector3(0.42f, 0.48f, 0.61f);
-
-	public const string BorderColorString = "border_color";
-}
