fb721780d91aff74013337df153827bcd92dbca1
Yeongjong Lee <cleanlyj@naver.com>
2022-03-11 13:58:40 +0900

    runtime: introduce Rotation Control
    Add nodes that control the Player Rotation.
    You can select it in the editor.

---
+++ MREGodotRuntime/Scripts/Controls/RotationControls/MouseRotationControl.cs
@@ -0,0 +1,49 @@
+using Godot;
+
+namespace Assets.Scripts.Control
+{
+    internal class MouseRotationControl : Spatial, IRotationControl
+    {
+        private bool cameraMove = false;
+        private Vector2 mouseDelta = new Vector2();
+        private Camera mainCamera;
+
+        public float CameraSpeed { get; set; } = 0.0015f;
+
+        public MouseRotationControl(Camera camera)
+        {
+            mainCamera = camera;
+        }
+
+        public override void _PhysicsProcess(float delta)
+        {
+            if (Input.IsActionPressed("ui_cancel"))
+            {
+                cameraMove = false;
+            }
+            if (!cameraMove) return;
+
+            if (!Input.IsActionPressed("shift"))
+            {
+                var newRotation = mainCamera.Rotation;
+                newRotation.x -= mouseDelta.y * CameraSpeed;
+                newRotation.y -= mouseDelta.x * CameraSpeed;
+                mainCamera.Rotation = newRotation;
+            }
+            mouseDelta = Vector2.Zero;
+        }
+
+        public override void _Input(InputEvent inputEvent)
+        {
+            if (inputEvent is InputEventMouseMotion e)
+            {
+                mouseDelta = e.Relative;
+            }
+            else if (inputEvent is InputEventMouseButton mouseButton)
+            {
+                if (!cameraMove && mouseButton.Pressed)
+                    cameraMove = true;
+            }
+        }
+    }
+}
