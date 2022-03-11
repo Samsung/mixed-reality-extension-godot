baebb79c71e8ba1e12f3d45c04ea95c3a6eaf3f9
Yeongjong Lee <cleanlyj@naver.com>
2022-03-11 13:20:57 +0900

    runtime: introduce Position Control
    Add nodes that control the Player Position.
    You can select it in the editor.

---
+++ MREGodotRuntime/Scripts/Controls/PositionControls/VirtualGamepadPositionControl.cs
@@ -0,0 +1,47 @@
+using Godot;
+
+namespace Assets.Scripts.Control
+{
+    public class VirtualGamepadPositionControl : Spatial, IPositionControl
+    {
+        private Joystick2D gamepad;
+        private Camera mainCamera;
+
+        public float Speed { get; set; } = 0.07f;
+
+        public VirtualGamepadPositionControl(Camera camera)
+        {
+            mainCamera = camera;
+        }
+
+        private void _on_VirtualGamepadPositionControl_gamepad_changed(string gamepadPath)
+        {
+            if (gamepad != null)
+            {
+                gamepad.QueueFree();
+                var newGamepad = LoadGamepad(gamepadPath);
+                AddChild(newGamepad);
+
+                gamepad = newGamepad;
+            }
+        }
+
+        private Joystick2D LoadGamepad(string scenePath)
+        {
+            var gamepadScene = ResourceLoader.Load<PackedScene>(scenePath);
+            var gamepad = gamepadScene.Instance<Joystick2D>();
+            gamepad.MainCamera = mainCamera;
+            gamepad.Speed = Speed;
+            return gamepad;
+        }
+
+        public override void _Ready()
+        {
+            var player = GetParent<Player>();
+            var gamepad = LoadGamepad(player.GamePadScenePath);
+            AddChild(gamepad);
+
+            player.Connect(nameof(Player.gamepad_changed), this, nameof(_on_VirtualGamepadPositionControl_gamepad_changed));
+        }
+    }
+}
