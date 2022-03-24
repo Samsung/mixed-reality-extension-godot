77110740aa83451e0e4742938a3514bb728ea3f3
Yeongjong Lee <cleanlyj@naver.com>
2022-03-24 18:54:47 +0900

    lib: add api to register user payload
    Add an api that can register a new payload with the `PayloadType`
    attribute of the registry class.

---
+++ Toolkit/Payloads/ToolkitPayloadTypeRegistry.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using MixedRealityExtension.Messaging.Payloads;
+
+namespace MixedRealityExtension.Toolkit.Messaging.Payloads
+{
+	[PayloadType(typeof(CreateFromToolkit), "create-from-toolkit")]
+	[PayloadType(typeof(ToolkitUpdate), "toolkit-update")]
+	public class ToolkitPayloadTypeRegistry
+	{
+	}
+}
