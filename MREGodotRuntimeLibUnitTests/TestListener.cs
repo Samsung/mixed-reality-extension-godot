2f07c7d237cdfa17272a39891527e505d5a4605a
Yeongjong Lee <cleanlyj@naver.com>
2022-04-12 19:50:11 +0900

    test: add unit test project and template
    How to run unit test
    1. open godot project(MREGodotRuntimeLibUnitTest/project.godot)
    2. run Main.tscn

---
+++ MREGodotRuntimeLibUnitTests/TestListener.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Samsung Electronics Co., Ltd. All rights reserved.
+// Licensed under the MIT License.
+
+using Godot;
+using NUnit.Framework.Interfaces;
+
+namespace MREGodotRuntimeLib.Core.UnitTest
+{
+	// TODO: This class is created temprarily to prevent runtime exception of unit test. so, its implementation is empty.
+	//       The implementations can be added later if needed.
+	public class TestListener : ITestListener
+	{
+		public void SendMessage(TestMessage message)
+		{
+			//TODO
+		}
+
+		public void TestFinished(ITestResult result)
+		{
+			//TODO
+			GD.Print("Name : " + result.Name + ", ResultState : " + result.ResultState);
+		}
+
+		public void TestOutput(TestOutput output)
+		{
+			//TODO
+		}
+
+		public void TestStarted(ITest test)
+		{
+			//TODO
+		}
+	}
+}
